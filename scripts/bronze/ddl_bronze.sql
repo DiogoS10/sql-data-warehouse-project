ad9d0d18f89c6c768b100ad081dd95e856665fcb
Diogo Salgado <diogosalgado70@gmail.com>
2026-03-04 15:55:45 +0000

    Add 'bronze.' prefix to table names in SQL script

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -13,9 +13,9 @@ Script Purpose:
 USE bronze;
 
 -- CRM Tables
-DROP TABLE IF EXISTS crm_cust_info;
+DROP TABLE IF EXISTS bronze.crm_cust_info;
 
-CREATE TABLE crm_cust_info (
+CREATE TABLE bronze.crm_cust_info (
     cst_id INT,
     cst_key VARCHAR(50),
     cst_firstname VARCHAR(50),
@@ -25,9 +25,9 @@ CREATE TABLE crm_cust_info (
     cst_create_date DATE
 );
 
-DROP TABLE IF EXISTS crm_prd_info;
+DROP TABLE IF EXISTS bronze.crm_prd_info;
 
-CREATE TABLE crm_prd_info (
+CREATE TABLE bronze.crm_prd_info (
     prd_id INT,
     prd_key VARCHAR(50),
     prd_nm VARCHAR(50),
@@ -37,9 +37,9 @@ CREATE TABLE crm_prd_info (
     prd_end_dt DATETIME
 );
 
-DROP TABLE IF EXISTS crm_sales_details;
+DROP TABLE IF EXISTS bronze.crm_sales_details;
 
-CREATE TABLE crm_sales_details (
+CREATE TABLE bronze.crm_sales_details (
     sls_ord_num VARCHAR(50),
     sls_prd_key VARCHAR(50),
     sls_cust_id INT,
@@ -52,24 +52,24 @@ CREATE TABLE crm_sales_details (
 );
 
 -- ERP Tables
-DROP TABLE IF EXISTS erp_loc_a101;
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
 
-CREATE TABLE erp_loc_a101 (
+CREATE TABLE bronze.erp_loc_a101 (
     cid VARCHAR(50),
     cntry VARCHAR(50)
 );
 
-DROP TABLE IF EXISTS erp_cust_az12;
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
 
-CREATE TABLE erp_cust_az12 (
+CREATE TABLE bronze.erp_cust_az12 (
     cid VARCHAR(50),
     bdate DATE,
     gen VARCHAR(50)
 );
 
-DROP TABLE IF EXISTS erp_px_cat_g1v2;
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
 
-CREATE TABLE erp_px_cat_g1v2 (
+CREATE TABLE bronze.erp_px_cat_g1v2 (
     id VARCHAR(50),
     cat VARCHAR(50),
     subcat VARCHAR(50),
