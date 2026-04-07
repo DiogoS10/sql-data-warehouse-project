76c1e196188f78ab5480d29e6f3fa27c2fe408fb
Diogo Salgado <diogosalgado70@gmail.com>
2026-04-07 15:58:31 +0100

    Enhance data transformation logic in SQL script
    Added normalization for country codes and gender values, and handled future birthdates.

--- scripts/silver/transform_bronze_data.sql
+++ scripts/silver/transform_bronze_data.sql
@@ -128,19 +128,35 @@ INSERT INTO silver.erp_cust_az12 (
 		)
 		SELECT
 			CASE 
-				WHEN cid LIKE 'NAS%' THEN substring(TRIM(cid), 4, length(cid))
+				WHEN cid LIKE 'NAS%' THEN substring(TRIM(cid), 4, length(cid)) -- Remove 'NAS' prefix if present
 				ELSE cid
 			END cid,
 			CASE
 				WHEN bdate > NOW() THEN NULL
 				ELSE bdate
-			END bdate,
+			END bdate, -- Set future birthdates to NULL
 			CASE 
 				WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
 				WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
 				ELSE 'n/a'
-			END gen
+			END gen -- Normalize gender values and handle unknown cases
 		FROM bronze.erp_cust_az12;
+
+-- Loading silver.erp_loc_a101
+
+INSERT INTO silver.erp_loc_a101 (
+			cid,
+            cntry
+		)
+		SELECT 
+			REPLACE(cid, '-', '') as cid,
+			CASE
+				WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+				WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+				WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+				ELSE TRIM(cntry)
+			END AS cntry -- Normalize and Handle missing or blank country codes
+		FROM bronze.erp_loc_a101;
 
 
 
